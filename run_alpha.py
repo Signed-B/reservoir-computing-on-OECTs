9f87853eb8bc5b126d4aec8d2d9fb38fedb1956f
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-07-09 11:20:22 -0400

    parallelize `run_alpha.py`

--- run_alpha.py
+++ run_alpha.py
@@ -1,37 +1,176 @@
-import shelve
+import os
 import time
+from copy import deepcopy
 
 import numpy as np
-from scipy.stats import uniform
+from joblib import Parallel, delayed
+from scipy.stats import norm, uniform
 
 from src import *
 
+
+def run_OECT_prediction(
+    signal_fname,
+    prediction_fname,
+    u0,
+    n,
+    p,
+    r_dist,
+    parameters,
+    alpha,
+    training_time,
+    testing_time,
+):
+    dt = 0.01
+    w_in_sigma = 0.004
+
+    D = len(u0)
+    sigma = 10
+    rho = 28
+    beta = 8 / 3
+
+    u0 = u0.copy()
+
+    # OECT parameters
+    Vdinit, R, Rg, Cg, Vp, Kp, W, L = generate_OECT_parameters(n, parameters)
+
+    A = erdos_renyi_network(n, p, r_dist)
+
+    w_in = input_layer(n, D, w_in_sigma)
+
+    w_out, u0, r0, V1_0 = train_oect_reservoir(
+        u0,
+        training_time,
+        dt,
+        w_in,
+        A,
+        alpha,
+        Vdinit,
+        R,
+        Rg,
+        Cg,
+        Vp,
+        Kp,
+        W,
+        L,
+        lorenz,
+        sigma=sigma,
+        rho=rho,
+        beta=beta,
+    )
+
+    signal, prediction = run_oect_reservoir_autonomously(
+        u0,
+        r0,
+        V1_0,
+        testing_time,
+        dt,
+        w_in,
+        w_out,
+        A,
+        Vdinit,
+        R,
+        Rg,
+        Cg,
+        Vp,
+        Kp,
+        W,
+        L,
+        lorenz,
+        sigma=sigma,
+        rho=rho,
+        beta=beta,
+    )
+
+    np.savetxt(signal_fname, signal)
+    np.savetxt(prediction_fname, prediction)
+    print("Single run completed!", flush=True)
+
+
+def run_tanh_prediction(
+    signal_fname,
+    prediction_fname,
+    u0,
+    n,
+    p,
+    r_dist,
+    alpha,
+    training_time,
+    testing_time,
+):
+    dt = 0.01
+    w_in_sigma = 0.004
+
+    D = len(u0)
+    sigma = 10
+    rho = 28
+    beta = 8 / 3
+
+    u0 = u0.copy()
+
+    A = erdos_renyi_network(n, p, r_dist)
+
+    w_in = input_layer(n, D, w_in_sigma)
+
+    ## train_reservoir
+    w_out, u0, r = train_reservoir(
+        u0,
+        A,
+        training_time,
+        dt,
+        w_in,
+        alpha,
+        lorenz,
+        0,
+        sigma=sigma,
+        rho=rho,
+        beta=beta,
+    )
+
+    ## Run reservoir autonomously.
+    signal, prediction = run_reservoir_autonomously(
+        u0,
+        r,
+        A,
+        testing_time,
+        dt,
+        w_in,
+        w_out,
+        lorenz,
+        0,
+        sigma=sigma,
+        rho=rho,
+        beta=beta,
+    )
+    np.savetxt(signal_fname, signal)
+    np.savetxt(prediction_fname, prediction)
+    print("Single run completed!", flush=True)
+
+
+n_processes = len(os.sched_getaffinity(0))
+
 output = "./Data/may8/alpha_ensemble"
 
-iterations = 10
+iterations = 100
 
-dim = 100
-# alphas = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
+n = 100
 alphas = [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
 
 training_time = 300  # training time/
-# training_time = 100
 testing_time = 100
 dt = 0.01
-
-ntraining = int(training_time / dt)
-ntesting = int(testing_time / dt)
 
 w_in_sigma = 0.004
 
 gateR = 2.7e4
 gateC = 8.98e-7
+pinchoff = -0.6
 
 parameters = dict()
 parameters["transconductance"] = {"mean": 0.582e-3, "stddev": 0.0582e-3}
 parameters["channel-width"] = {"mean": 200e-6, "stddev": 0}
 parameters["channel-length"] = {"mean": 101e-6, "stddev": 0}
-parameters["threshold-voltage"] = {"mean": -0.6, "stddev": 0}  # pinch-off voltage
+parameters["pinchoff-voltage"] = {"mean": pinchoff, "stddev": 0}  # pinch-off voltage
 parameters["weighting-resistor"] = {"mean": 500, "stddev": 100}
 parameters["gate-capacitance"] = {"mean": gateC, "stddev": 0.1 * gateC}
 parameters["gate-resistance"] = {"mean": gateR, "stddev": 0.1 * gateR}
@@ -39,8 +178,12 @@ parameters["applied-drain-voltage"] = {"mean": -0.05, "stddev": 0}
 
 # system
 D = 3
-dist = uniform(100, 500)
-
+r_dist = uniform(100, 500)
+delta_dist = norm(scale=0.005)
+p = 6 / n
+sigma = 10
+rho = 28
+beta = 8 / 3
 
 ensemble_results = []
 
@@ -49,183 +192,54 @@ print("run_alpha.py: BEGIN ENSEMBLE RUNS")
 
 initt = time.time()
 
-ics = [[-7.4, -11.1, 20] + np.random.normal(size=3) * 0.05 for _ in range(iterations)]
+u0 = generate_initial_conditions(
+    iterations,
+    [-7.4, -11.1, 20],
+    delta_dist,
+    5000,
+    0.1,
+    lorenz,
+    sigma=10,
+    rho=28,
+    beta=8 / 3,
+)
+print("Initial conditions generated!", flush=True)
 
-
-for iter in range(iterations):
-    print(f"========== Iteration {iter}/{iterations} ==========")
-    st = time.time()
-
-    # ==== OECT ====
-    # print("OECT data generation.")
-    # parameters for lorenz
-    sigma = 10
-    rho = 28
-    beta = 8 / 3
-
-    x = ics[iter][0]
-    y = ics[iter][1]
-    z = ics[iter][2]
-    u = [x, y, z]
-
-    # relax to attractor
-    for t in range(5000):
-        u += dt * lorenz(u, t, sigma, rho, beta)
-
-    OECT_signals = []
-    OECT_predictions = []
-
-    print("> Generating OECT data...")
-    for alpha in alphas:
-        n = dim
-        u0 = u.copy()
-        print("> Alpha", alpha)
-
-        # OECT parameters
-        Vdinit, R, Rg, Cg, Vp, Kp, W, L = generate_OECT_parameters(n, parameters)
-
-        A = erdos_renyi_network(n, 6 / n, dist)
-
-        w_in = w_in_sigma * (2.0 * np.random.rand(n, D) - np.ones((n, D)))
-
-        w_out, u0, r0, V1_0 = train_oect_reservoir(
-            u0,
-            training_time,
-            dt,
-            w_in,
-            A,
-            alpha,
-            Vdinit,
-            R,
-            Rg,
-            Cg,
-            Vp,
-            Kp,
-            W,
-            L,
-            lorenz,
-            sigma=sigma,
-            rho=rho,
-            beta=beta,
+arglist = []
+for alpha in alphas:
+    for i in range(iterations):
+        arglist.append(
+            (
+                f"Data/Alpha/{alpha}_{i}_OECT_signal.csv",
+                f"Data/Alpha/{alpha}_{i}_OECT_prediction.csv",
+                u0[i].copy(),
+                n,
+                p,
+                r_dist,
+                deepcopy(parameters),
+                alpha,
+            )
         )
 
-        # print("\n")
 
-        signal, prediction = run_oect_reservoir_autonomously(
-            u0,
-            r0,
-            V1_0,
-            testing_time,
-            dt,
-            w_in,
-            w_out,
-            A,
-            Vdinit,
-            R,
-            Rg,
-            Cg,
-            Vp,
-            Kp,
-            W,
-            L,
-            lorenz,
-            sigma=sigma,
-            rho=rho,
-            beta=beta,
+Parallel(n_jobs=n_processes)(delayed(run_OECT_prediction)(*arg) for arg in arglist)
+
+
+arglist = []
+for alpha in alphas:
+    for i in range(iterations):
+        arglist.append(
+            (
+                f"Data/Alpha/{alpha}_{i}_tanh_signal.csv",
+                f"Data/Alpha/{alpha}_{i}_tanh_prediction.csv",
+                u0[i].copy(),
+                n,
+                p,
+                r_dist,
+                alpha,
+                training_time,
+                testing_time,
+            )
         )
 
-        OECT_signals.append(signal)
-        OECT_predictions.append(prediction)
-
-    # ==== tanh ====
-    # print("Tanh data generation.")
-
-    tanh_signals = []
-    tanh_predictions = []
-
-    tanshift = 0
-
-    # # parameters for lorenz
-    # sigma = 10
-    # rho = 28
-    # beta = 8 / 3
-
-    # x = -7.4
-    # y = -11.1
-    # z = 20
-    # u = [x, y, z]
-
-    # # relax to attractor
-    # for t in range(5000):
-    #     u += dt * lorenz(u, t, sigma, rho, beta)
-
-    print("> Generating tanh data...")
-    for alpha in alphas:
-        n = dim
-        u0 = u.copy()
-        print("> Alpha", alpha)
-
-        A = erdos_renyi_network(n, 6 / n, dist)
-
-        w_in = w_in_sigma * (2.0 * np.random.rand(n, D) - np.ones((n, D)))
-
-        ## train_reservoir
-        w_out, u0, r = train_reservoir(
-            n,
-            D,
-            u0,
-            A,
-            ntraining,
-            dt,
-            w_in,
-            alpha,
-            lorenz,
-            tanshift,
-            sigma=sigma,
-            rho=rho,
-            beta=beta,
-        )
-
-        ## Run reservoir autonomously.
-        signal_during_auto, pred_during_auto = run_reservoir_autonomously(
-            n,
-            D,
-            u0,
-            r,
-            A,
-            ntraining,
-            ntesting,
-            dt,
-            w_in,
-            w_out,
-            lorenz,
-            tanshift,
-            sigma=sigma,
-            rho=rho,
-            beta=beta,
-        )
-
-        tanh_signals.append(signal_during_auto)
-        tanh_predictions.append(pred_during_auto)
-
-    en_result = {
-        "OECT_signals": OECT_signals,
-        "OECT_predictions": OECT_predictions,
-        "tanh_signals": tanh_signals,
-        "tanh_predictions": tanh_predictions,
-    }
-
-    ensemble_results.append(en_result)
-
-    print(
-        f"> Time elapsed: {time.time() - st:.2f} s, total time: {time.time() - initt:.2f} s"
-    )
-
-
-# END ENSEMBLE
-
-
-with shelve.open(f"{output}/data") as data:
-    data["dicts"] = ensemble_results
-    data["time"] = np.arange(0, testing_time, dt)
-    data["alphas"] = alphas
+Parallel(n_jobs=n_processes)(delayed(run_tanh_prediction)(*arg) for arg in arglist)
