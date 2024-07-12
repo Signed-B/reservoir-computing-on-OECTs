dc435ba2d5833b1bc08cb0e235d5b0f183b7f8e9
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-07-12 10:08:18 -0400

    Update collect_timeseries.py

--- collect_timeseries.py
+++ collect_timeseries.py
@@ -1,17 +1,19 @@
 import json
-import shelve
 
-dimindex = 1  # index in reservoir_dims of the dimension to plot
+n = 25
+r = 1
 
-with shelve.open("Data/may8/dims_ensemble/data") as data:
-    datadicts = data["dicts"]
-    reservoir_dims = data["dims"]
-    t = data["time"]
+with open("Data/Dims/{n}_{r}_OECT.json") as oect_data:
+    reservoir_dims = oect_data["n"]
+    t = oect_data["t"]
+    OECT_signal = oect_data["signal"]
+    OECT_prediction = oect_data["prediction"]
 
-OECT_signal = datadicts[0]["OECT_signals"][dimindex]
-OECT_prediction = datadicts[0]["OECT_predictions"][dimindex]
-tanh_signal = datadicts[0]["tanh_signals"][dimindex]  # same as other signal
-tanh_prediction = datadicts[0]["tanh_predictions"][dimindex]
+with open("Data/Dims/{n}_{r}_tanh.json") as oect_data:
+    reservoir_dims = oect_data["n"]
+    t = oect_data["t"]
+    tanh_signal = oect_data["signal"]
+    tanh_prediction = oect_data["prediction"]
 
 d = {}
 d["signal-x"] = [sig[0] for sig in OECT_signal]
