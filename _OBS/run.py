2d982732e1df4d5d7651e4b71c61099823988e9a
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-07-16 14:54:32 -0400

    remove unused code

--- _OBS/run.py
+++
@@ -1,9 +0,0 @@
-# create file that can be run with arguments from command line
-
-import argparse
-
-parser = argparse.ArgumentParser(
-    prog="OECT Simulator",
-    description="Simulate OECTs with reservoir computing",
-    epilog="Enjoy the program! :)",
-)
